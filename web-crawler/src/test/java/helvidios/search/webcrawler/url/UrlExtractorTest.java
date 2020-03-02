8c246c3434524017ace730ecac11e74425fc9a1e
Yemelyanov Andrey (Consultant) <andrey.yemelyanov@consultant.volvo.com>
2020-03-02 13:25:21 +0100

    sync

--- web-crawler/src/test/java/helvidios/search/webcrawler/url/UrlExtractorTest.java
+++ web-crawler/src/test/java/helvidios/search/webcrawler/url/UrlExtractorTest.java
@@ -1,6 +1,8 @@
 package helvidios.search.webcrawler.url;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+
 import org.junit.Test;
 import helvidios.search.webcrawler.HtmlDocument;
 import helvidios.search.webcrawler.TestUtil;
@@ -11,7 +13,8 @@ import static org.junit.Assert.assertThat;
 public class UrlExtractorTest {
     @Test
     public void extractUrls() throws IOException, Exception {
-        HtmlDocument doc = new HtmlDocument("http://www.mycrawler.net/dir1/dir2/page.html", TestUtil.readFile("page1.html"));
+        HtmlDocument doc = new HtmlDocument("http://www.mycrawler.net/dir1/dir2/page.html",
+                TestUtil.readFile("page1.html"));
 
         UrlExtractor extractor = new SimpleUrlExtractor();
         List<String> urls = extractor.getUrls(doc);
@@ -24,12 +27,21 @@ public class UrlExtractorTest {
 
     @Test
     public void extractUrlsWithPrefix() throws IOException, Exception {
-        HtmlDocument doc = new HtmlDocument("http://www.mycrawler.net/dir1/dir2/page.html", TestUtil.readFile("page1.html"));
+        HtmlDocument doc = new HtmlDocument("http://www.mycrawler.net/dir1/dir2/page.html",
+                TestUtil.readFile("page1.html"));
 
         UrlExtractor extractor = new SimpleUrlExtractor("https://www.w3schools.com");
         List<String> urls = extractor.getUrls(doc);
         List<String> expected = Arrays.asList("https://www.w3schools.com/html/");
         Collections.sort(expected);
         assertThat(urls, is(expected));
+    }
+
+    @Test
+    public void noMathchingUrls() throws IOException, URISyntaxException, Exception {
+        HtmlDocument doc = new HtmlDocument("http://www.mycrawler.net/dir1/dir2/page.html", TestUtil.readFile("page1.html"));
+        UrlExtractor extractor = new SimpleUrlExtractor("https://www.google.com");
+        List<String> urls = extractor.getUrls(doc);
+        assertThat(urls.isEmpty(), is(true));
     }
 }
