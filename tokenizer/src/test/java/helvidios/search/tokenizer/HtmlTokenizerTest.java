325d78442e09b0a00aca4641656da6eb859a33d7
unknown <andrey.yemelyanov@gmail.com>
2020-04-04 11:16:08 +0200

    sync

--- tokenizer/src/test/java/helvidios/search/tokenizer/HtmlTokenizerTest.java
+++ tokenizer/src/test/java/helvidios/search/tokenizer/HtmlTokenizerTest.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class HtmlTokenizerTest {
     @Test
-    public void tokenizeHtmlPage() throws Exception {
+    public void tokenizeJavaDocsPages() throws Exception {
         Arrays.asList(
             "Collectors.html",
             "DateFormat.html",
@@ -19,6 +19,21 @@ public class HtmlTokenizerTest {
             assertThat(tokens, is(not(empty())));
             //System.out.println(tokens);
         });
+    }
+
+    @Test
+    public void tokenizeHtmlPage(){
+        String html = readFile("page1.html");
+        Tokenizer tokenizer = new HtmlTokenizer();
+        List<String> tokens = tokenizer.getTokens(html);
+        assertThat(tokens, is(Arrays.asList(
+            "link", "titles", "title", "attribute", 
+            "specifies", "extra", "information", "element", 
+            "information", "often", "shown", "tooltip", "text", 
+            "mouse", "moves", "element", "visit", "html", "tutorial", 
+            "html", "tutorial", "link1", "visit", "facebook", "tutorial"
+        )));
+        System.out.println(tokens);
     }
 
     private String readFile(String path) {
