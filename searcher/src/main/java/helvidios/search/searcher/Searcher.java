211159974b59570e427416bc9c940094b3f21715
Yemelyanov Andrey (Consultant) <andrey.yemelyanov@consultant.volvo.com>
2020-06-22 15:29:08 +0200

    search algorithm outline

--- searcher/src/main/java/helvidios/search/searcher/Searcher.java
+++ searcher/src/main/java/helvidios/search/searcher/Searcher.java
@@ -2,10 +2,15 @@ package helvidios.search.searcher;
 
 import org.apache.logging.log4j.Logger;
 import helvidios.search.index.Index;
+import helvidios.search.index.Posting;
 import helvidios.search.index.storage.IndexRepository;
 import helvidios.search.linguistics.Lemmatizer;
+import helvidios.search.storage.DocId;
+import helvidios.search.storage.DocumentRepository;
+import helvidios.search.storage.HtmlDocument;
 import helvidios.search.tokenizer.Tokenizer;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Searcher {
     
@@ -13,16 +18,19 @@ public class Searcher {
     private final Logger log;
     private final Tokenizer tokenizer;
     private final Lemmatizer lemmatizer;
+    private final DocumentRepository docRepo;
 
     /**
      * Initializes a new instance of the searcher.
      * @param indexRepo implementation of {@link IndexRepository} interface which provides access to the index
+     * @param docRepo document repository where raw documents are stored
      * @param tokenizer query tokenizer
      * @param lemmatizer query lemmatizer
      * @param log log component
      */
     public Searcher(
         IndexRepository indexRepo,
+        DocumentRepository docRepo,
         Tokenizer tokenizer,
         Lemmatizer lemmatizer, 
         Logger log){
@@ -30,6 +38,7 @@ public class Searcher {
         this.index = new Index(indexRepo, log);
         this.tokenizer = tokenizer;
         this.lemmatizer = lemmatizer;
+        this.docRepo = docRepo;
     }
 
     /**
@@ -60,7 +69,46 @@ public class Searcher {
     }
 
     private List<Match> getMatches(List<String> terms, int k){
-        return Arrays.asList();
+
+        Map<String, Double> queryWeights = weights(terms);
+        Map<Integer, Double> scores = new HashMap<>();
+        
+        // accumulate scores
+        for(String term : terms){
+            List<Posting> postings = index.postingsList(term);
+            for(Posting posting : postings){
+                scores.put(
+                    posting.docId(), 
+                    scores.getOrDefault(posting.docId(), 0.0) + (queryWeights.get(term) * posting.tfIdfScore())
+                );
+            }
+        }
+
+        // length-normalize scores
+        //....
+
+        // return K top matches
+        return scores.keySet().stream()
+                              .map(docId -> buildMatch(docId, scores.get(docId)))
+                              .sorted((m1, m2) -> Double.compare(m1.score(), m2.score()))
+                              .limit(k)
+                              .collect(Collectors.toList());
+    }
+
+    private Match buildMatch(int docId, double documentScore){
+        HtmlDocument doc = docRepo.get(new DocId(docId));
+        return new Match.Builder()
+                        .docId(docId)
+                        .documentUrl(doc.getUrl())
+                        .documentContent(doc.getContent())
+                        .documentTitle(doc.getTitle())
+                        .documentScore(documentScore)
+                        .build();
+    }
+
+    private Map<String, Double> weights(List<String> terms){
+        Map<String, Double> weights = new HashMap<>();
+        return weights;
     }
 
 }
