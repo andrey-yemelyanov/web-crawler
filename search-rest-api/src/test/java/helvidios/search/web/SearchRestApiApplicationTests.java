ffaafaea183764c3fccf02693bd9729fe12267ab
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2020-06-24 22:46:06 +0200

    sync

---
+++ search-rest-api/src/test/java/helvidios/search/web/SearchRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package helvidios.search.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
