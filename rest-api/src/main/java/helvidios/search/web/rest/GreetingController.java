deaf227f6c62475d1439b80191089908af97f111
Yemelyanov Andrey (Consultant) <andrey.yemelyanov@consultant.volvo.com>
2020-06-24 16:14:02 +0200

    sync

--- rest-api/src/main/java/helvidios/search/web/rest/GreetingController.java
+++ rest-api/src/main/java/helvidios/search/web/rest/GreetingController.java
@@ -4,6 +4,18 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import helvidios.search.index.storage.IndexRepository;
+import helvidios.search.index.storage.MongoDbIndexRepository;
+import helvidios.search.linguistics.ApacheNlpLemmatizer;
+import helvidios.search.linguistics.Lemmatizer;
+import helvidios.search.storage.DocumentRepository;
+import helvidios.search.storage.MongoDbDocumentRepository;
+import helvidios.search.tokenizer.HtmlTokenizer;
+import helvidios.search.tokenizer.Tokenizer;
+import java.util.stream.*;
 
 @RestController
 public class GreetingController {
@@ -12,10 +24,11 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
     
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
-        // IndexRepository indexRepo = new MongoDbIndexRepository.Builder()
-        //                                                       .setDatabase(dbName)
-        //                                                       .build();
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public List<String> greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        IndexRepository indexRepo = new MongoDbIndexRepository.Builder()
+                                                              .setDatabase("javadocs-search-db")
+                                                              .build();
+        
+        return indexRepo.getVocabulary().stream().map(term -> term.name()).collect(Collectors.toList());
     }
 }
